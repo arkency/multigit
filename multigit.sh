419c7f492706ac14581223ffaf20579200ef9c5a
Tomasz Wróbel <tomekwr@gmail.com>
2020-06-18 15:07:40 +0200

    Allow running non-git commands, display boxes
    Breaking change: previously git command was assummed.
    
    Now you need to do `multigit git status`, for which you may wanna have a
    custom alias.

--- multigit.sh
+++ multigit.sh
@@ -1,1 +1,25 @@
-function multigit { find .. -maxdepth 2 -mindepth 2 -type d -name ".git"  -exec dirname {} \; | while read repodir; do echo "-- REPO: $repodir"; git -C $repodir $*; done; }
+function multigit    { find .. -maxdepth 2 -mindepth 2 -type d -name ".git"  -exec dirname {} \; | while read repodir; do mg_printrepo $repodir; cd $repodir; $*; cd - >/dev/null; done; }
+function multigitbox { find .. -maxdepth 2 -mindepth 2 -type d -name ".git"  -exec dirname {} \; | while read repodir; do mg_printbox  $repodir; cd $repodir; $*; cd - >/dev/null; done; }
+
+function mg_printline {
+  echo '---------------------------------------------'
+}
+
+function mg_printpad {
+  line='----------------------------------------'
+  PROC_NAME="$*"
+  PROC_NAME="${PROC_NAME/$'\n'}"
+  printf "%s %s %s\n" "---" $PROC_NAME "${line:${#PROC_NAME}}"
+}
+
+function mg_printbox {
+  mg_printline
+  mg_printline
+  mg_printpad "$*"
+  mg_printline
+  mg_printline
+}
+
+function mg_printrepo {
+  echo "-- REPO: $*"
+}
